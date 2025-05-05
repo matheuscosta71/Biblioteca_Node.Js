2afc5c114bcab78680e77e2e020d01e1ccc9564d
matheuscosta71 <matheushenri26@outlook.com>
2025-05-04 21:30:21 -0300

    Utilizando o mongoose para conectar com Mongodb

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+
+const conexao= await conectaNaDatabase();
+
+conexao.on("error", (erro)=>{
+    console.error("erro de conexao", erro); 
+})
+
+conexao.once("open", () =>{
+    console.log("conexao realizada com sucesso");
+})
 
 const app = express(); 
 app.use(express.json());
@@ -51,3 +62,4 @@ app.delete("/livros/:id", (req, res) =>{
 } )
 
 export default app;
+
