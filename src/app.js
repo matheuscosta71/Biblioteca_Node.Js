84c0038325c97e4542b8ef956b4daae1e9565163
matheuscosta71 <matheushenri26@outlook.com>
2025-05-14 23:41:45 -0300

    Implementação dos Controlers de autor

--- src/app.js
+++ src/app.js
@@ -1,65 +1,24 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
 
-const conexao= await conectaNaDatabase();
+const conexao = await conectaNaDatabase();
 
-conexao.on("error", (erro)=>{
-    console.error("erro de conexao", erro); 
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com o banco feita com sucesso");
 })
 
-conexao.once("open", () =>{
-    console.log("conexao realizada com sucesso");
-})
+const app = express();
+routes(app);
 
-const app = express(); 
-app.use(express.json());
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos aneis"
-    },
-    {
-        id: 2,
-        titulo : "O Hobbit"
-    },
-]
-
-function buscaLivro(id){
-    return livros.findIndex(livro=> {
-        return livro.id === Number (id);
-    })
-}
-
-app.get("/", (req, res) =>{
-    res.status(200).send ("Curso de Node. Js");
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
 });
-
-app.get("/livros", (req, res) =>{
-    res.status(200).json(livros);
-});
-
-app.get("/livros/:id", (req, res)=>{
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index])
-})
-
-app.post("/livros", (req, res)=>{
-    livros.push(req.body),
-    res.status(201).send("Livro Cadastrado com Sucesso!")
-});
-
-app.put("/livros/:id", (req, res)=>{
-    const index = buscaLivro(req.params.id);
-    livros[index]. titulo = req.body.titulo;
-    res.status(200).send("Livro alterado com sucesso")
-});
-
-app.delete("/livros/:id", (req, res) =>{
-    const index= buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200). send("Livro removido com suceso");
-} )
 
 export default app;
-
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -6,3 +6,4 @@ async function conectaNaDatabase() {
 };
 
 export default conectaNaDatabase;
+
